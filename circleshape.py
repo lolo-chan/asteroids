0f7eb23d29cbb5926060199324d8187332e4debc
Rohan <92527986+lolo-chan@users.noreply.github.com>
2025-05-05 04:10:05 +0000

    Create shoot feature

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, circle):
+        if self.radius + circle.radius >= pygame.Vector2.distance_to(self.position, circle.position):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
@@ -9,19 +13,36 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
+    group_updatable = pygame.sprite.Group()
+    group_drawable = pygame.sprite.Group()
+    Player.containers = (group_updatable, group_drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    group_asteroids = pygame.sprite.Group()
+    Asteroid.containers = (group_asteroids, group_drawable, group_updatable)
+
+    AsteroidField.containers = (group_updatable)
+    asteroidField = AsteroidField()
+
+    group_shots = pygame.sprite.Group()
+    Shot.containers = (group_shots, group_updatable, group_drawable)
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        group_updatable.update(dt)
+        for i in group_asteroids:
+            if player.collision(i):
+                print("Game over!")
+                sys.exit()
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        
+        for i in group_drawable:
+            i.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
