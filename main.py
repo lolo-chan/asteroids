bc162e0a9a8c649e0e0ac79d355d4e2542794e68
lolo-chan <rohanmchetan@gmail.com>
2025-12-12 03:37:10 +0000

    Add framerate settings

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while(True):
         log_state()
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
