234d966a9163f59ec33bff7b3cd05a1a49f1ca53
lolo-chan <rohanmchetan@gmail.com>
2025-12-12 16:54:07 +0000

    Create shot class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from logger import log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from logger import log_event
 import sys
 
@@ -17,8 +18,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, drawable, updatable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
