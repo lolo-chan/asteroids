db3f231f41436a95bf118319b816d62d992a4fa8
lolo-chan <rohanmchetan@gmail.com>
2025-12-12 17:02:17 +0000

    Add destruction to shot

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for j in shots:
+                if i.collides_with(j):
+                    log_event("asteroid_shot")
+                    i.kill()
+                    j.kill()
 
         screen.fill("black")
 
